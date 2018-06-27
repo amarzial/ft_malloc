b809a7c75bf6fc41b9da2825ec34779198a32497
Alessandro Marzialetti <setti93@gmail.com>
2018-06-27 13:12:32 +0200

    thread safe

--- src/manager.c
+++ src/manager.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <amarzial@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 11:09:26 by amarzial          #+#    #+#             */
-/*   Updated: 2018/06/27 11:57:18 by amarzial         ###   ########.fr       */
+/*   Updated: 2018/06/27 12:10:26 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,27 +45,30 @@ void	*new_zone(size_t size)
 
 void	*request_memory(size_t size)
 {
-	size_t	*mem;
 	t_clist	*chunk;
+	void	*retval;
 
 	if (size == 0)
 		return (NULL);
+	pthread_mutex_lock(&g_mutex);
+	retval = NULL;
 	if (size <= SMALL_SIZE)
 	{
 		chunk = size <= TINY_SIZE ? g_store.tiny_list : g_store.small_list;
-		while (chunk != NULL)
-		{
-			if ((mem = get_free(chunk, size)) != NULL)
-				return (mem);
+		while (chunk != NULL && (retval = get_free(chunk, size)) == NULL)
 			chunk = chunk->next;
+		if (retval == NULL)
+		{
+			chunk = new_zone(size);
+			retval = get_free(chunk, size);
 		}
-		chunk = new_zone(size);
-		return (get_free(chunk, size));
 	}
 	else
 	{
-		return (request_big(&g_store.big_list, size));
+		retval = request_big(&g_store.big_list, size);
 	}
+	pthread_mutex_unlock(&g_mutex);
+	return (retval);
 }
 
 void	*reallocate_memory(void *ptr, size_t size)
@@ -85,8 +88,10 @@ void	*reallocate_memory(void *ptr, size_t size)
 
 void	deallocate_memory(void *ptr)
 {
+	pthread_mutex_lock(&g_mutex);
 	if (!mem_free(ptr, g_store.tiny_list) && !mem_free(ptr, g_store.small_list))
 	{
 		free_big(ptr, &g_store.big_list);
 	}
+	pthread_mutex_unlock(&g_mutex);
 }
